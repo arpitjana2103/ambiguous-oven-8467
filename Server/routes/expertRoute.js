c44b6eaa2c4585ddc1270f89c1c0e20ed4492520
Arpit Jana <arpitjana2103@gmail.com>
2023-08-26 22:10:25 +0530

    API Completed

--- Server/routes/expertRoute.js
+++ Server/routes/expertRoute.js
@@ -1,12 +1,11 @@
-const { Router } = require("express");
-const { ExpertModel } = require("../models/expert.model");
-const { client } = require("../db/redis");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
+const {Router} = require('express');
+const {ExpertModel} = require('../models/expert.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const expertRouter = Router();
-expertRouter.get("/", async (req, res) => {
+expertRouter.get('/', async (req, res) => {
     try {
         let data = await ExpertModel.find();
         let arr = data.map((el) => {
@@ -17,25 +16,26 @@ expertRouter.get("/", async (req, res) => {
                 gender: el.gender,
                 email: el.email,
                 image: el.image,
-                specialisation: el.specialisation
-            }
-        })
+                specialisation: el.specialisation,
+            };
+        });
         res.status(200).send({
             isError: false,
-            message: "request successfull",
-            data: arr
-        })
+            message: 'request successfull',
+            length: arr.length,
+            data: arr,
+        });
     } catch (error) {
         res.status(400).send({
             isError: true,
-            error: error.message
-        })
+            error: error.message,
+        });
     }
-})
-expertRouter.get("/:specialisation", async (req, res) => {
-    let { specialisation } = req.params;
+});
+expertRouter.get('/:specialisation', async (req, res) => {
+    let {specialisation} = req.params;
     try {
-        let data = await ExpertModel.find({ specialisation });
+        let data = await ExpertModel.find({specialisation});
         let arr = data.map((el) => {
             return {
                 id: el._id,
@@ -44,109 +44,110 @@ expertRouter.get("/:specialisation", async (req, res) => {
                 gender: el.gender,
                 email: el.email,
                 image: el.image,
-                specialisation: el.specialisation
-            }
-        })
+                specialisation: el.specialisation,
+            };
+        });
         res.status(200).send({
             isError: false,
-            message: "request successfull",
-            data: arr
-        })
+            length: arr.length,
+            message: 'request successfull',
+            data: arr,
+        });
     } catch (error) {
         res.status(400).send({
             isError: true,
-            error: error.message
-        })
-    }
-})
-
-expertRouter.post("/register", async (req, res) => {
-    const { name, image, age, gender, email, password, specialisation } = req.body;
-    try {
-        const user = await ExpertModel.findOne({ email });
-        if (!user) {
-            bcrypt.hash(password, Number(process.env.salt), async (err, hash) => {
-                if (err) {
-                    return res.status(400).send({
-                        isError: true,
-                        error: err
-                    })
-                }
-                const new_user = new ExpertModel({ name, image, age, gender, email, password: hash, specialisation });
-                await new_user.save();
-                return res.status(200).send({
-                    isError: false,
-                    message: "user registered successfully",
-                    user: new_user
-                })
-            })
-        } else {
-            return res.status(400).send({
-                isError: true,
-                error: "user already exist"
-            })
-        }
-    } catch (error) {
-        return res.status(400).send({
-            isError: true,
-            error: error.message
+            error: error.message,
         });
     }
-})
+});
 
-expertRouter.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const user = await ExpertModel.findOne({ email });
-        if (user) {
-            if (bcrypt.compareSync(password, user.password)) {
-                const token = jwt.sign({ expertID: user._id, email: user.email }, process.env.token, { expiresIn: "24h" })
-                return res.status(200).send({
-                    isError: false,
-                    message: "expert logged in successfully",
-                    token
-                })
-            }
-        } else {
-            return res.status(400).send({
-                isError: true,
-                error: "this email doesn't exist please register first"
-            });
-        }
-    } catch (error) {
-        return res.status(400).send({
-            isError: true,
-            error: error.message
-        });
-    }
-})
+// expertRouter.post("/register", async (req, res) => {
+//     const { name, image, age, gender, email, password, specialisation } = req.body;
+//     try {
+//         const user = await ExpertModel.findOne({ email });
+//         if (!user) {
+//             bcrypt.hash(password, Number(process.env.salt), async (err, hash) => {
+//                 if (err) {
+//                     return res.status(400).send({
+//                         isError: true,
+//                         error: err
+//                     })
+//                 }
+//                 const new_user = new ExpertModel({ name, image, age, gender, email, password: hash, specialisation });
+//                 await new_user.save();
+//                 return res.status(200).send({
+//                     isError: false,
+//                     message: "user registered successfully",
+//                     user: new_user
+//                 })
+//             })
+//         } else {
+//             return res.status(400).send({
+//                 isError: true,
+//                 error: "user already exist"
+//             })
+//         }
+//     } catch (error) {
+//         return res.status(400).send({
+//             isError: true,
+//             error: error.message
+//         });
+//     }
+// })
 
-expertRouter.post("/logout", async (req, res) => {
-    const { email } = req.body;
-    let str = req.headers.authorization;
-    str = str.split(" ");
-    let token = str[1];
-    if (token) {
-        try {
-            await client.set(`${email}expertblacktoken`, token);
-            return res.status(200).send({
-                isError: false,
-                message: 'logout successfull'
-            })
-        } catch (error) {
-            console.log(error)
-            return res.status(400).send({
-                isError: true,
-                error: error.message
-            });
-        }
-    }
-    else {
-        return res.status(400).send({
-            isError: true,
-            error: "not valid request"
-        });
-    }
-})
+// expertRouter.post("/login", async (req, res) => {
+//     const { email, password } = req.body;
+//     try {
+//         const user = await ExpertModel.findOne({ email });
+//         if (user) {
+//             if (bcrypt.compareSync(password, user.password)) {
+//                 const token = jwt.sign({ expertID: user._id, email: user.email }, process.env.token, { expiresIn: "24h" })
+//                 return res.status(200).send({
+//                     isError: false,
+//                     message: "expert logged in successfully",
+//                     token
+//                 })
+//             }
+//         } else {
+//             return res.status(400).send({
+//                 isError: true,
+//                 error: "this email doesn't exist please register first"
+//             });
+//         }
+//     } catch (error) {
+//         return res.status(400).send({
+//             isError: true,
+//             error: error.message
+//         });
+//     }
+// })
 
-module.exports = {expertRouter}
+// expertRouter.post("/logout", async (req, res) => {
+//     const { email } = req.body;
+//     let str = req.headers.authorization;
+//     str = str.split(" ");
+//     let token = str[1];
+//     if (token) {
+//         try {
+//             await client.set(`${email}expertblacktoken`, token);
+//             return res.status(200).send({
+//                 isError: false,
+//                 message: 'logout successfull'
+//             })
+//         } catch (error) {
+//             console.log(error)
+//             return res.status(400).send({
+//                 isError: true,
+//                 error: error.message
+//             });
+//         }
+//     }
+//     else {
+//         return res.status(400).send({
+//             isError: true,
+//             error: "not valid request"
+//         });
+//     }
+// })
+
+module.exports = {expertRouter};
--- Server/routes/userRoute.js
+++ Server/routes/userRoute.js
@@ -1,114 +1,103 @@
 const express = require('express');
 const {UserModel} = require('../models/user.model');
-const {client} = require('../db/redis');
+const {blackListModel} = require('../models/blacklist.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const userRouter = express.Router();
 
-userRouter.post('/register', async (req, res) => {
-    const {name, age, gender, email, password} = req.body;
+userRouter.post('/register', async function (req, res) {
     try {
-        const user = await UserModel.findOne({email});
-        if (!user) {
-            bcrypt.hash(
-                password,
-                Number(process.env.salt),
-                async (err, hash) => {
-                    if (err) {
-                        return res.status(400).send({
-                            isError: true,
-                            error: err,
-                        });
-                    }
-                    const new_user = new UserModel({
-                        name,
-                        age,
-                        gender,
-                        email,
-                        password: hash,
-                    });
-                    await new_user.save();
-                    return res.status(200).send({
-                        isError: false,
-                        message: 'user registered successfully',
-                        user: new_user,
-                    });
-                }
-            );
-        } else {
-            return res.status(400).send({
-                isError: true,
-                error: 'user already exist',
-            });
-        }
-    } catch (error) {
-        return res.status(400).send({
-            isError: true,
-            error: error.message,
+        const {name, email, age, gender, password} = req.body;
+        bcrypt.hash(password, 5, async function (err, hash) {
+            if (!err) {
+                const newUser = new UserModel({
+                    name,
+                    email,
+                    age,
+                    gender,
+                    password: hash,
+                });
+                await newUser.save();
+
+                return res.status(200).json({
+                    status: 'ok',
+                    message: 'A new user registered',
+                    user: req.body,
+                });
+            } else {
+                return res.status(400).json({
+                    status: 'fial',
+                    error: err.message,
+                });
+            }
+        });
+    } catch (err) {
+        return res.status(400).json({
+            status: 'fail',
+            error: err.message,
         });
     }
 });
 
 userRouter.post('/login', async (req, res) => {
-    const {email, password} = req.body;
     try {
+        const {email, password} = req.body;
         const user = await UserModel.findOne({email});
-        if (user) {
-            if (bcrypt.compareSync(password, user.password)) {
-                const token = jwt.sign(
-                    {userID: user._id, email: user.email},
-                    process.env.token,
-                    {expiresIn: '24h'}
-                );
-                return res.status(200).send({
-                    isError: false,
-                    message: 'user logged in successfully',
-                    token,
-                    data: {
-                        name: user.name,
-                        email: user.email,
-                    },
+        if (!user) throw new Error('User not Found');
+        bcrypt.compare(password, user.password, function (err, result) {
+            if (err) {
+                return res.status(400).json({
+                    status: 'fail',
+                    error: err.message,
                 });
             }
-        } else {
-            return res.status(400).send({
-                isError: true,
-                error: "this email doesn't exist please register first",
-            });
-        }
+
+            if (result) {
+                const aToken = jwt.sign(
+                    {userID: user._id, userName: user.name},
+                    'masaiA',
+                    {
+                        expiresIn: '1h',
+                    }
+                );
+                return res.status(200).json({
+                    status: 'ok',
+                    message: 'Login Successfull',
+                    accessToken: aToken,
+                });
+            }
+
+            if (!result) {
+                return res.status(200).json({
+                    status: 'fail',
+                    Error: 'Wrong Password',
+                });
+            }
+        });
     } catch (error) {
-        return res.status(400).send({
-            isError: true,
+        return res.status(400).json({
+            status: 'fail',
             error: error.message,
         });
     }
 });
 
 userRouter.post('/logout', async (req, res) => {
-    const {email} = req.body;
-    let str = req.headers.authorization;
-    str = str.split(' ');
-    let token = str[1];
-    if (token) {
-        try {
-            await client.set(`${email}blacktoken`, token);
-            return res.status(200).send({
-                isError: false,
-                message: 'logout successfull',
-            });
-        } catch (error) {
-            console.log(error);
-            return res.status(400).send({
-                isError: true,
-                error: error.message,
-            });
-        }
-    } else {
-        return res.status(400).send({
-            isError: true,
-            error: 'not valid request',
+    const token = req.headers.authorization?.split(' ')[1];
+    try {
+        if (!token) throw new Error('Token not found !');
+        const newToken = await new blackListModel({token});
+        newToken.save();
+        return res.status(200).json({
+            status: 'ok',
+            message: 'Logout Successfull',
+        });
+    } catch (error) {
+        return res.status(400).json({
+            status: 'fail',
+            error: error.message,
         });
     }
 });
